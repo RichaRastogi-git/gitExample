64734b9f86be13cd47f9eb4d6e90643c4f0fdf9c
RichaRastogi-git <78945174+RichaRastogi-git@users.noreply.github.com>
2021-04-21 04:18:57 -0500

    Updated Employee

--- Employee.java
+++ Employee.java
@@ -2,6 +2,6 @@ public class Employee{
 private String name = "Ramesh";
 public static void main(String k[]){
  Employee emp = new Employee();
- System.out.println("Employee Name"+emp.name);
+ System.out.println("Employee Name: "+emp.name);
 }
 }
